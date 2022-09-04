5562bca8ebb7262c12c1e5fae82106fc4a9a8fa5
Ivan Miller <therealigmiller@gmail.com>
2022-09-04 16:39:58 +0300

    feat: add new component

---
+++ react-typescript/src/components/TodoContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoContainer {
+    render() {
+        return (
+            <div>
+                <h1>Hello from React App</h1>
+                <p>I am in react app</p>
+            </div>
+        );
+    }
+}
+
+export default TodoContainer;
